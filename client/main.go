d431ab502ee14022ce5ff993fa78da42a7ce7897
Gonzalo Muñoz <gonzalo.munoz0@walmart.com>
2022-07-06 17:15:46 -0400

    Client calling grpc

---
+++ client/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"goProtogRPC/testpb"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	cc, err := grpc.Dial("localhost:5070", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := testpb.NewTestServiceClient(cc)
+	//DoUnary(c)
+	DoClientStreaming(c)
+}
+
+func DoUnary(c testpb.TestServiceClient) {
+	req := &testpb.GetTestRequest{Id: "t1"}
+	res, err := c.GetTest(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("error while calling GetTest: %v", err)
+	}
+	log.Printf("response from server: %v", res)
+}
+
+func DoClientStreaming(c testpb.TestServiceClient) {
+	questions := []*testpb.Question{
+		{
+			Id:       "q8t1",
+			Answer:   "Azul",
+			Question: "Color related to Go",
+			TestId:   "t1",
+		},
+		{
+			Id:       "q9t1",
+			Answer:   "Google",
+			Question: "Company that created Go",
+			TestId:   "t1",
+		},
+		{
+			Id:       "q10t1",
+			Answer:   "Backend",
+			Question: "Go is Back or Front language",
+			TestId:   "t1",
+		},
+	}
+
+	stream, err := c.SetQuestions(context.Background())
+	if err != nil {
+		log.Fatalf("error while calling SetQuestions: %v", err)
+	}
+	for _, question := range questions {
+		log.Println("sending question: ", question.Id)
+		stream.Send(question)
+		time.Sleep(2 * time.Second)
+	}
+	msg, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("error while receiving response: %v", err)
+	}
+	log.Printf("response from server: %v", msg)
+}
